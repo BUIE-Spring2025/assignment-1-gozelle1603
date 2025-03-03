751f9fe1ca217b77b36d78b4fbde21e17d94d430
gozelle1603 <hatice.gozelle@std.bogazici.edu.tr>
2025-03-03 14:47:34 +0300

    I changed the main.py

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 def int_to_roman(num):
-    """
-    Convert an integer to a Roman numeral.
+    roman_letters = {
+        1000: "M", 900: "CM", 500: "D", 400: "CD",
+        100: "C", 90: "XC", 50: "L", 40: "XL",
+        10: "X", 9: "IX", 5: "V", 4: "IV", 1: "I"
+    }
 
-    :param num: Integer value between 1 and 3999 inclusive.
-    :return: A string representing the Roman numeral of the integer.
-    """
+    roman_value = []
+    
+    for value, symbol in roman_letters.items():
+        count = num // value  
+        roman_value.append(symbol * count)  
+        num %= value 
+
+    return "".join(roman_value)
+
+
+
